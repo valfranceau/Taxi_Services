1db9820aeb10736515c98518e0227a01ac570f88
DOINE Jacquenson <doinejacquenson@gmail.com>
2021-03-03 13:14:59 -0500

    test

--- app/src/main/java/com/example/taxi_motoservices/MainActivity.java
+++ app/src/main/java/com/example/taxi_motoservices/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.taxi_motoservices;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toast.makeText(MainActivity.this, "Firebase Connection Success", Toast.LENGTH_SHORT).show();
     }
 }
